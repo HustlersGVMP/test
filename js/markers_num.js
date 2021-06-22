54a67e9446e53d8353ee3520f0ef71373e916575
flowster543 <86293019+flowster543@users.noreply.github.com>
2021-06-22 18:30:36 +0200

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -1,5 +1,5 @@
   L.marker([-2574.75, 6485.5], {icon: cardIcon1}).addTo(map)
-    .bindPopup("<h2>Insel 1</h3><p><img =scr'https://i.imgur.com/3sx9iLU.jpeg' data-lightbox='image-1' data-title='Insel 1'>Open In-Game Screenshot</a></p><div class='input-group input-group-sm mb-3'><div class='input-group-append'></div></div></p>");
+    .bindPopup("<h2>Insel 1</h3><p><img scr="https://i.imgur.com/3sx9iLU.jpeg">Open In-Game Screenshot</a></p><div class='input-group input-group-sm mb-3'><div class='input-group-append'></div></div></p>");
   L.marker([-617, 3907.25], {icon: cardIcon1}).addTo(map)
     .bindPopup("<h2>Insel 2</h3><p><a href='http://epvpimg.com/ZbhMbab' data-lightbox='image-1' data-title='Insel 2'>Open In-Game Screenshot</a></p><div class='input-group input-group-sm mb-3'><input type='text' class='form-control' id='copy' value='https://rockstarnews.ru/map/#5/-2574.75/6485.5'><div class='input-group-append'><button class='btn btn-outline-secondary copy' data-clipboard-target='#copy' type='button'>Copy</button></div></div></p>");
   L.marker([-7242.5, 5172.25], {icon: cardIcon1}).addTo(map)
