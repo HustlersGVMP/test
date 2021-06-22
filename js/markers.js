0d5b49e0e15e9c7837b7ab57e387878584e7df47
flowster543 <86293019+flowster543@users.noreply.github.com>
2021-06-22 14:17:42 +0200

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,2 +1,2 @@
-  L.marker([-1171.5, 3826], {icon: cardMarker}).addTo(map)
+  L.marker([-7473, 4573], {icon: cardMarker}).addTo(map)
     .bindPopup("Inside Los Santos Customs<br><a href='https://content.rockstarnews.ru/up/images/4797cc9dfe41a8c7808e0e5fd8a795c4.png' data-lightbox='image-1' data-title='Inside Los Santos Customs'>Open In-Game Screenshot</a>");
