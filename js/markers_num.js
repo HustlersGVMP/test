4552fe2a12b8f075544748cd48a09ff573418594
flowster543 <86293019+flowster543@users.noreply.github.com>
2021-10-05 13:15:17 +0200

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -1,3 +1,5 @@
+L.marker([-7600, 6400], {icon: cardIcon7}).addTo(map)
+  .bindPopup("<h2>cayo perico all</h3><p><a href='https://i.imgur.com/qWKxp4R.png' data-lightbox='image-1' data-title='cayo perico all'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-2766.5, 3400], {icon: cardIcon4}).addTo(map)
   .bindPopup("<h2>Canyon1</h3><p><a href='https://i.imgur.com/uVOaexK.png' data-lightbox='image-1' data-title='Canyon1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-2379, 3256.25], {icon: cardIcon1}).addTo(map)
